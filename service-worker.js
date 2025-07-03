2a6c80b39a2f136b311b5e9f27c41bcee48040f2
mferraretto <matheus.ferraretto94@gmail.com>
2025-07-03 09:34:39 -0300

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,58 +1,31 @@
-const CACHE_NAME = 'casa-rosa-cache-v3' ;
- 
-const ATIVOS_ESTÁTICOS = [
- 
-  './' ,
-  './index.html' ,
-  './manifest.json' ,
-  './icon-192.png' ,
-  './icon-512.png'
-];
+const CACHE_NAME = "casa-rosa-cache-v1";
 
-self.addEventListener ( 'instalar' , evento = >
- {
-  evento. waitUntil (
-    caches. aberto ( CACHE_NAME ). então ( cache => cache. addAll ( STATIC_ASSETS ))
+self.addEventListener("install", function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
   );
 });
 
-self.addEventListener ( 'ativar '
- , evento = > {
-  evento .waitUntil​(
-    caches. chaves (). então ( chave s =>
-      Prom ise . all (chaves. filtro ( k => k !== CACHE_NAME ). mapa ( k => caches. excluir(k)))
+self.addEventListener("activate", event => {
+  event.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
     )
   );
 });
 
-self.addEventListener ( 'buscar' , evento = >
- {
-  se ( requestURL.origem === localização.origem ) {
-    evento. respondWith (
-      caches. correspondência (evento. solicitação ). então ( cached => {
-        se (em cache) retornar em cache;
-        retornar buscar (evento. solicitação ). então ( resposta => {
- 
-          const respClone = resposta. clone ();
-          se (evento. solicitação . url . começaCom ( 'http' )) {
-            caches. aberto ( NOME_DO_CACHE ). então ( cache => cache. put (evento. solicitação , respClone));
-          }
-          retornar resposta ;
-        });
-      })
-    );
-  } outro {
-    evento. respondWith (
-      buscar (evento. solicitação )
-        . então ( resposta => {
-          const respClone = resposta. clone ();
-          se (evento. solicitação . url . começaCom ( 'http' )) {
-            caches. aberto ( NOME_DO_CACHE ). então ( cache => cache. put (evento. solicitação , respClone));
-          }
-          retornar resposta ;
-​
-        })
-        . catch ( () => caches. match (evento. solicitação))
-    );
-  }
+self.addEventListener("fetch", function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
 });
